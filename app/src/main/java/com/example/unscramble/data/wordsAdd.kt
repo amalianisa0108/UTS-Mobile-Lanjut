eeab0e94f2438a03568727eb5fbb96dc3dc9e537
amalianisa0108 <nisaamalia0108@gmail.com>
2026-04-16 15:46:08 +0700

    Menambahkankan file WordsAdd.kt untuk entitiy

---
+++ app/src/main/java/com/example/unscramble/data/wordsAdd.kt
@@ -0,0 +1,10 @@
+package com.example.unscramble.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "words")
+data class wordsAdd (
+    @PrimaryKey(autoGenerate = true) val id:Int =0,
+    val word: String
+)
