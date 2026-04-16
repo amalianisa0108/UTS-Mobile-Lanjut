99f6bf6604ff24c07fbd433c31528b53da5bf1d7
amalianisa0108 <nisaamalia0108@gmail.com>
2026-04-16 20:04:34 +0700

    Menambahkan companion object pada file wordsAddDatabase.kt

--- app/src/main/java/com/example/unscramble/data/wordsAddDatabase.kt
+++ app/src/main/java/com/example/unscramble/data/wordsAddDatabase.kt
@@ -1,9 +1,27 @@
 package com.example.unscramble.data
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [wordsAdd::class], version = 1)
 abstract class wordsAddDatabase : RoomDatabase(){
     abstract fun wordsDao(): wordsDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE:wordsAddDatabase? = null
+        fun getInstance(context: Context): wordsAddDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    wordsAddDatabase::class.java,
+                    "word_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
 }
